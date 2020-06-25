23b76240d94d1f1b5f78329387b2110745ea4b52
TomB34 <tomballantyne34@gmail.com>
2020-06-25 15:12:54 +0100

    Boss room added randomly. Isn't adjacent to start room. checkSides function added.

--- mapGenerator.py
+++ mapGenerator.py
@@ -1,6 +1,45 @@
 import random
 
-#TODO: create a 5x5 map that randomly generates a sequence of connected rooms
+#Function for checking all four sides of a room to see if they are empty.
+def checkSides(fullMapList, adjRoomCoords):   #Takes full map, adjRoom coords
+    emptyRooms = []
+    # Check North
+    if adjRoomCoords[1] == 0:
+        roomToCheck = [adjRoomCoords[0], adjRoomCoords[1] + 4]
+    else:
+        roomToCheck = [adjRoomCoords[0], adjRoomCoords[1] - 1]
+
+    if fullMapList[roomToCheck[1]][roomToCheck[0]] == 0:
+        emptyRooms.append(roomToCheck)
+
+    # Check East
+    if adjRoomCoords[0] == 4:
+        roomToCheck = [adjRoomCoords[0] - 4, adjRoomCoords[1]]
+    else:
+        roomToCheck = [adjRoomCoords[0] + 1, adjRoomCoords[1]]
+    if fullMapList[roomToCheck[1]][roomToCheck[0]] == 0:
+        emptyRooms.append(roomToCheck)
+
+    # Check South
+    if adjRoomCoords[1] == 4:
+        roomToCheck = [adjRoomCoords[0], adjRoomCoords[1] - 4]
+    else:
+        roomToCheck = [adjRoomCoords[0], adjRoomCoords[1] + 1]
+    if fullMapList[roomToCheck[1]][roomToCheck[0]] == 0:
+        emptyRooms.append(roomToCheck)
+
+    # Check West
+    if adjRoomCoords[0] == 0:
+        roomToCheck = [adjRoomCoords[0] + 4, adjRoomCoords[1]]
+    else:
+        roomToCheck = [adjRoomCoords[0] - 1, adjRoomCoords[1]]
+    if fullMapList[roomToCheck[1]][roomToCheck[0]] == 0:
+        emptyRooms.append(roomToCheck)
+
+    return emptyRooms
+    # Returns a list of empty rooms that are available to be used
+
+#-----------------------------------------------------------------------------------------------------
 
 #create a blank 5x5 2d array
 fullMap = []
@@ -11,83 +50,56 @@ for y in range(5):
 
 #Pick a random starting point (represented by '1')
 
-coList = [] # List to store coords. Used to randomly pick from for room generations
-coList.append([random.randint(0, 4), random.randint(0, 4)]) #Adds random square to list to serve as start room
-fullMap[coList[0][1]][coList[0][0]] = 1 # Sets coords of first room to '1'
-#print(coList)
+# List to store coords. Used to randomly pick from for room generations
+coList = []
+#Startroom represented by '1' to be the spawn room
+startRoom = [random.randint(0, 4), random.randint(0, 4)]
+coList.append(startRoom)
+fullMap[coList[0][1]][coList[0][0]] = 1
 
-for y in range(5):
-    print(fullMap[y])
-
-
-#Create array of random room types || 0 = empty coord, 1 = start room, 2 = empty room, 3 = enemy room, 4 = loot room, 5 = boss room
-roomTypeList = []
-for x in range(random.randint(5, 10)):
-    roomTypeList.insert(0, random.randint(2, 4))
-
+#Create array of random room types
+# 0 = empty coord, 1 = start room, 2 = empty room, 3 = enemy room, 4 = loot room, 5 = boss room
+roomTypeList = [3, 4]
+for x in range(random.randint(5, 7)):
+    roomTypeList.append(random.randint(2, 4))
 
 # Pick random room from coList for new room to adjoin to
 for i in range(len(roomTypeList)):
-
     roomFound = False
     while roomFound == False:
         adjRoom = coList[random.randint(0, len(coList) - 1)]
-        print("adjRoom: " + str(adjRoom))
         roomToCheck = []
-        emptyRooms = []
 
-        #TODO: check all directions for empty room. Add empty rooms to a list
-
-        # Check North
-        if adjRoom[1] == 0:
-            roomToCheck = [adjRoom[0], adjRoom[1] + 4]
-        else:
-            roomToCheck = [adjRoom[0], adjRoom[1] - 1]
-
-        if fullMap[roomToCheck[1]][roomToCheck[0]] == 0:
-            emptyRooms.append(roomToCheck)
-
-        # Check East
-        if adjRoom[0] == 4:
-            roomToCheck = [adjRoom[0] - 4, adjRoom[1]]
-        else:
-            roomToCheck = [adjRoom[0] + 1, adjRoom[1]]
-        if fullMap[roomToCheck[1]][roomToCheck[0]] == 0:
-            emptyRooms.append(roomToCheck)
-
-        # Check South
-        if adjRoom[1] == 4:
-            roomToCheck = [adjRoom[0], adjRoom[1] - 4]
-        else:
-            roomToCheck = [adjRoom[0], adjRoom[1] + 1]
-        if fullMap[roomToCheck[1]][roomToCheck[0]] == 0:
-            emptyRooms.append(roomToCheck)
-
-        # Check West
-        if adjRoom[0] == 0:
-            roomToCheck = [adjRoom[0] + 4, adjRoom[1]]
-        else:
-            roomToCheck = [adjRoom[0] -1, adjRoom[1]]
-        if fullMap[roomToCheck[1]][roomToCheck[0]] == 0:
-            emptyRooms.append(roomToCheck)
+        emptyRooms = checkSides(fullMap, adjRoom)
 
         # If no rooms are found, change adjRoom
         if emptyRooms == []:
             adjRoom = coList[random.randint(0, len(coList) - 1)]
         else:
-            roomFound = True
+                roomFound = True    #Breaks from while loop, allows for next room to be made
+                #pick a room at random to from the list to fill. Add to coList.
+                roomToFill = random.choice(emptyRooms)
+                coList.append(roomToFill)
+                fullMap[roomToFill[1]][roomToFill[0]] = roomTypeList.pop()
+                #print("List of full rooms: " + str(coList) + "\n")
 
-            print("Empty rooms to pick from: " + str(emptyRooms))
+#TODO: Add boss room, not adjacent to start room
+bossRoomFound = False
+while bossRoomFound == False:
+    adjRoom = coList[random.randint(1, len(coList) - 1)]    #Start at index 1 to miss out start room
+    roomToCheck = []
 
-            #TODO: pick a room at random to fill. Add to coList.
-            roomToFill = random.choice(emptyRooms)
-            coList.append(roomToFill)
-            fullMap[roomToFill[1]][roomToFill[0]] = roomTypeList.pop()
-            print("List of full rooms: " + str(coList) + "\n")
+    emptyRooms = checkSides(fullMap, adjRoom)
 
-        for y in range(5):
-            print(fullMap[y])
+    if emptyRooms == []:
+        adjRoom = coList[random.randint(1, len(coList) - 1)]
+    else:
+        bossRoomFound = True
+        roomToFill = random.choice(emptyRooms)
+        coList.append(roomToFill)
+        fullMap[roomToFill[1]][roomToFill[0]] = 5
 
-
+for y in range(5):
+    print(fullMap[y])
 
 
