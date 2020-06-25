eff22c060e7eedb30e55f0441b4c1f632353049f
TomB34 <tomballantyne34@gmail.com>
2020-06-25 15:29:13 +0100

    Commited unsaved files to branch

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!python3
 # textAdventureGame.py - My First Text Adventure Game
 
+import mapGenerator as mapGen
+
 #0 = empty coord, 1 = start room, 2 = empty room, 3 = enemy room, 4 = loot room, 5 = boss room
 
-#Create map
+mapGen.printMap(mapGen.fullMap)
+
+currentRoom = mapGen.startRoom
+print(currentRoom)
 
 
 
